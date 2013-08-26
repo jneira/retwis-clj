86acb06201f6a1798216faed10259d920fb7e6a9
jneira <atreyu.bbb@gmail.com>
2013-08-27 00:56:37 +0200

    ''

--- project.clj
+++ project.clj
@@ -4,15 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [ring "1.1.8"]
+                 [ring "1.2.0"]
                  [compojure "1.1.5"]
                  [stencil "0.3.2"]
                  [com.taoensso/carmine "2.0.0-RC1"]
                  [com.taoensso/tower "1.7.1"]
-                 [digest "1.3.0"]
+                 [digest "1.4.3"]
                  [com.novemberain/validateur "1.5.0"]]
-  :plugins [[lein-ring "0.8.3"]
-            [lein-cloudbees "1.0.4"]]
+  :plugins [[lein-ring "0.8.3"]]
   :ring {:handler retwis-clj.app/site-handler}
   :war-resources-path "resources/public"
   :main retwis-clj.server
